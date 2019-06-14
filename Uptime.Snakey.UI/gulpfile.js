c2793fd20ce086732cff71221fe4b6308f5829bc
Kamelija Sekulovska <kamelija.sekulovska@uptime.mk>
2019-06-14 16:52:54 +0200

    Added gulpfile.js for watching scss files

---
+++ Uptime.Snakey.UI/gulpfile.js
@@ -0,0 +1,17 @@
+﻿/// <binding AfterBuild='sass' ProjectOpened='sass:watch' />
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('wwwroot/css/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('wwwroot/css/**/*.scss', gulp.series('sass'));
+});
